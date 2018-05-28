cac2a040cf9a878438b03d1664b67e0c40d55ab7
刘聪 <1097318622@qq.com>
2018-05-28 14:48:44 +0800

    欢迎页面-倒计时（5s）

--- src/pages/startup/startup.ts
+++ src/pages/startup/startup.ts
@@ -19,35 +19,21 @@ export class StartupPage {
   public num = 5;
   public timer:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    console.log('init',this.num);
 
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StartupPage');
-    // this.go();
-    // this.timer=
-  }
-  // ngOnInit(){
-  //
-  // }
+    window.clearInterval(this.timer);
+    this.timer=setInterval(()=>{
+      this.num--;
 
-//   setInterval(() => {
-//   this.num = this.num - 1;
-//   if (this.num <= 0){
-//   clearInterval(this.timer);
-//   this.navCtrl.push(AccountPage)
-// }
-// console.log(this.num)
-// }, 1000);
-//     go(){
-//         setInterval(function () {
-//           this.num = 20;
-//           this.num = this.num - 1;
-//           console.log('num999',this.num);
-//
-//         },1000)
-//      }
+      if(this.num===0){
+        window.clearInterval(this.timer);
+        this.gohome();
+      }
+    },1000);
+  }
+
   gohome() {
     this.navCtrl.setRoot(AccountPage);
   }
