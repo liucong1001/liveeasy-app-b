5af4cebae2f2182b74c6147a42d5bd1e29b860cd
瞿俏 <568012359@qq.com>
2018-06-23 18:33:58 +0800

    更新报单列表

--- src/pages/home/declaration/declardetail/declardetail.ts
+++ src/pages/home/declaration/declardetail/declardetail.ts
@@ -19,12 +19,14 @@ export class DeclardetailPage {
   users:any;
   orderDetail:any;
   allOrder:any;
+  feelist:any;
   constructor(public navCtrl: NavController, public navParams: NavParams,private homeProvider:HomeProvider,) {
     this.orderid=navParams.get('item').orderId;
     console.log(this.orderid);
     this.homeProvider.decldetail(this.orderid).then(res=>{
       this.allOrder=res.data;
       this.orderDetail=res.data.order;
+      this.feelist=res.data.feeList;
       this.users=res.data.user;
       console.log(this.allOrder);
     });
@@ -33,8 +35,29 @@ export class DeclardetailPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad DeclardetailPage');
   }
-goInfo(){
-    this.navCtrl.push(DeclinfoPage)
+  numJOSN=[
+    {name:'报单客户',val:0},
+    {name:'报单房源',val:1},
+    {name:'业主信息',val:2},
+    {name:'合同信息',val:3},
+    {name:'收款信息',val:4},
+    {name:'业绩类型',val:5},
+    {name:'补充协议',val:6},
+  ];
+goInfo(val){
+    this.navCtrl.push(DeclinfoPage,{
+      val:val,
+      order:this.allOrder,
+    })
 }
+
+  paypipe(val){
+    for(var i in this.numJOSN){
+      if(val == this.numJOSN[i].val){
+        return this.numJOSN[i].name;
+      }
+    }
+  }
+
 
 }
--- src/pages/home/declaration/declardetail/declinfo/declinfo.ts
+++ src/pages/home/declaration/declardetail/declinfo/declinfo.ts
@@ -14,12 +14,68 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'declinfo.html',
 })
 export class DeclinfoPage {
-
+  feelist:any;
+  order:any;
+  customer:any;
+  nums:any;
+  orderics:any;
+  propertyinfo:any;
+  orders:any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.nums=navParams.get('val');
+    console.log(this.nums)
+    this.order=navParams.get('order');
+    this.orders=navParams.get('order').order;
+    this.feelist=navParams.get('order').feeList;
+    this.customer=navParams.get('order').order.customer;
+    this.propertyinfo=navParams.get('order').order.propertyInfo;
+    // JSON.parse(this.orderics=navParams.get('order').order.orderPics);
+    // console.log(JSON.parse(this.orderics));
+    console.log(this.customer)
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DeclinfoPage');
   }
+//报单客户
+  censusJSON=[
+    {name:'本地',val:1},
+    {name:'外地',val:2},
+    {name:'港澳台',val:3},
+    {name:'外籍',val:3},
+  ]
+  censuspipe(val){
+    for(var i in this.censusJSON){
+      if(val == this.censusJSON[i].val){
+        return this.censusJSON[i].name;
+      }
+    }
+  }
 
+//收款信息——费用类型
+  typeJSON=[
+    {name:'居间服务费',val:1},
+    {name:'按揭服务费',val:2},
+    {name:'代办服务费',val:2},
+  ]
+  statpipe(val){
+    for(var i in this.typeJSON){
+      if(val == this.typeJSON[i].val){
+        return this.typeJSON[i].name;
+      }
+    }
+  }
+
+  //收款信息——付款方
+  payJSON=[
+    {name:'业主',val:1},
+    {name:'客户',val:2},
+  ]
+  paypipe(val){
+    for(var i in this.payJSON){
+      if(val == this.payJSON[i].val){
+        return this.payJSON[i].name;
+      }
+    }
+  }
 }
